8a535ab8050cd1c568fc5081a7856152ec6b2bde
briannaordaz <briannaordaz5@gmail.com>
2025-02-02 23:30:09 -0800

    added author, course, faculty, and purpose

---
+++ COMP003A.LectureActivity4-2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace COMP003A.LectureActivity4_2;
+//Author: Brianna Ordaz
+//Course: Comp003A
+//Faculty: Jonathan Cruz
+//Purpose: Practice implementing methods in C#
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
